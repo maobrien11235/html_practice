107f00afced5f207d8d5858e222ca385de5dcc4d
Matthew O'Brien <matthew.obrien@regions.com>
2017-10-04 07:53:04 -0500

    Started executing the w3schools 'Company' theme. Stopped at the Add a Carousel section.

--- actions.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", ()=>{
-    var name = prompt("What is your name?");
-    document.getElementById("hello_world").innerHTML="Welcome to Our Coding Workshop " + name + "!";
-
-    
-})
